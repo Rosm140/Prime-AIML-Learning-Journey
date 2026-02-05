f3ce35403f9cbcd8d76602263068422cadad1906
Rohit Mahadhane <rohitmahadane83@gmail.com>
2026-02-06 01:03:29 +0530

    Delete 01_Python_Basics/login.py

--- 01_Python_Basics/login.py
+++
@@ -1,9 +0,0 @@
-username = input("Enter Username\n")
-password = input("Enter password\n")
-
-if (username == "rohit" and password == "pass123"):
-    print("LOGIN SUCCESSFULL")
-elif (username != "rohit"):
-    print("Invalid username..!")
-else:
-    print("Wrong password..!")
