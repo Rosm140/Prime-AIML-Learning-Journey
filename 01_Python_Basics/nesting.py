ac607641f2ef177def246d6aefb786fa36953fed
Rohit Mahadhane <rohitmahadane83@gmail.com>
2026-02-07 00:07:22 +0530

    nested stmt added

---
+++ 01_Python_Basics/nesting.py
@@ -0,0 +1,10 @@
+usename = input("enter username: ")
+password = input("enter password ")
+
+if(usename == "rohit" and password == "pass123"):
+    print("Login Successfully..")
+else:
+    if(usename !="rohit"):
+        print("Wrong username.")
+    else:
+        print("wrong password.")
